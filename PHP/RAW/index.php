dbc7df12217db60388baf4ebef9164b017c402b4
AR-Shahin <mdshahinmije96@gmail.com>
2024-02-08 21:31:29 +0600

    first class

---
+++ PHP/RAW/index.php
@@ -0,0 +1,36 @@
+<?php
+
+// This a comment!
+
+/**
+ * Multiline comment
+ */
+// echo "Hello World!";
+
+
+$varName = 10;
+$varName = 10.5;
+// $varName = "Shahin";
+
+
+// echo gettype((bool)$varName);
+
+// print "hhh";
+
+// var_dump($varName);
+
+// print_r($varName);
+
+// $a = "A";
+// $b = "B";
+// $A = "10";
+// echo $a . $b . "<br>";
+// echo '<h1>Hello</h1>';
+// echo 10 + 10;
+
+
+
+$a = 10;
+// $a ++;
+echo --$a;
+echo $a;
